ae9aeaf8f2107c27b643f370602bf14e64dfab2a
duduvalle77 <duduvalle77@yahoo.com.br>
2018-07-12 17:36:48 -0300

    testes realizados do phat 700 1 e 3

--- Grafos_Template_2018_1/main.cpp
+++ Grafos_Template_2018_1/main.cpp
@@ -6,35 +6,33 @@ int main(int argc, char **argv){
 
 
     Grafo grafo4 = Grafo();
-    Grafo grafo5 = Grafo();
-    Grafo grafo9 = Grafo();
+    //Grafo grafo9 = Grafo();
 
     grafo4.readFile2("instancia/p_hat700-3.txt"); // ja rodou alfa 0.1 e guloso
-    grafo5.readFile2("instancia/p_hat700-1.txt");
-    grafo9.readFile2("instancia/keller5.txt");
+    //grafo9.readFile2("instancia/keller5.txt");
 
-    /*cout << "DSJC500_5 guloso :" << endl;
-    grafo6.acharCliqueMaxima1();
-    cout << "DSJC500_5 randomizado alfa 0.1" << endl;
+    cout << "p_hat700-3 guloso :" << endl;
+    grafo4.acharCliqueMaxima1();
+    cout << "p_hat700-3 randomizado alfa 0.1" << endl;
     for(int i = 0; i < 10; i++)
     {
-        grafo6.acharCliqueMaxima(0.1,1000);
+        grafo4.acharCliqueMaxima(0.1,500);
     }
-    cout << "DSJC500_5 randomizado alfa 0.2" << endl;
+    cout << "p_hat700-3 randomizado alfa 0.2" << endl;
     for(int i = 0; i < 10; i++)
     {
-        grafo6.acharCliqueMaxima(0.2,1000);
+        grafo4.acharCliqueMaxima(0.2,500);
     }
-    cout << "DSJC500_5 randomizado alfa 0.3" << endl;
+    cout << "p_hat700-3 randomizado alfa 0.3" << endl;
     for(int i = 0; i < 10; i++)
     {
-        grafo6.acharCliqueMaxima(0.3,1000);
+        grafo4.acharCliqueMaxima(0.3,500);
     }
-    cout << "DSJC500_5 randomizado reativo:" << endl;
+    cout << "p_hat700-3 randomizado reativo:" << endl;
     for(int i = 0; i < 10; i++)
     {
-        grafo6.gulosoRandomizadoReativo(10,1,500,3000);
-    }*/
+        grafo4.gulosoRandomizadoReativo(10,1,250,1500);
+    }
     return 0;
 }
 
